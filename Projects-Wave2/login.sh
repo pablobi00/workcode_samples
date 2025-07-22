c3d1a6438c489dd903bb52428a2266da03129bff
Pablo Barcenas <pablobi00@gmail.com>
2025-07-21 19:03:14 -0700

    Update login.sh

--- Projects-Wave2/login.sh
+++ Projects-Wave2/login.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# use - modify to use variables ./login.sh admin Kundera001
+# use - modify to use variables ./login.sh admin -----
 
 curl -v \
  --header "Accept: application/json, text/plain, */*" \
  --header "Content-Type: application/json;charset=UTF-8" \
- --data '{"username":"admin","password":"Kundera001"}' \
+ --data '{"username":"admin","password":"-----"}' \
  --insecure \
  https://localhost:8443/api/authenticate/login
 
